1d4aa014a2b5aec3af110bdbbb7f657bd9de3c1d
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2022-04-03 11:22:22 +0800

    New Theme

--- blog.config.js
+++ blog.config.js
@@ -9,7 +9,7 @@ const BLOG = {
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
 
-  THEME: process.env.NEXT_PUBLIC_THEME || 'next', // 主题， 支持 ['next','hexo',"fukasawa','medium']
+  THEME: process.env.NEXT_PUBLIC_THEME || 'medium', // 主题， 支持 ['next','hexo',"fukasawa','medium']
   THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
   LANG: 'zh-HK', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   HOME_BANNER_IMAGE: './bg_image.jpg', // 首页背景大图，默认文件：/public/bg_image.jpg 。会被Notion中的封面图覆盖。
--- themes/medium/config_medium.js
+++ themes/medium/config_medium.js
@@ -1,7 +1,7 @@
 const CONFIG_MEDIUM = {
 
   POST_LIST_COVER: true, // 文章列表显示图片封面
-  POST_LIST_PREVIEW: true, // 列表显示文章预览
+  POST_LIST_PREVIEW: false, // 列表显示文章预览
   POST_LIST_CATEGORY: true, // 列表显示文章分类
   POST_LIST_TAG: true, // 列表显示文章标签
 
@@ -9,10 +9,10 @@ const CONFIG_MEDIUM = {
   POST_DETAIL_TAG: true, // 文章显示标签
 
   // 菜单
-  MENU_ABOUT: true, // 显示关于
+  MENU_ABOUT: false, // 显示关于
   MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_TAG: false, // 显示标签
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
   // Widget
